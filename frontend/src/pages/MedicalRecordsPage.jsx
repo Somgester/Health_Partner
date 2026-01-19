39c780425dbf2de1a097c6ba0858af75abd76c28
Dhruv Ratmele <129955117+Somgester@users.noreply.github.com>
2026-01-19 17:04:13 +0530

    feat: add medical records page to render the medical records in separate page

---
+++ frontend/src/pages/MedicalRecordsPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MedicalRecordsList from '../components/PatientComponents/MedicalRecordsList';
+
+const MedicalRecordsPage = () => {
+  return <MedicalRecordsList />;
+};
+
+export default MedicalRecordsPage;
